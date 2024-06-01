487346a967d47195ae987c667f234fef7f27353c
darkstar331 <harman6549@gmail.com>
2024-06-01 17:39:24 +0530

    Initial commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Spinner from './Iphone-spinner-2.gif'
+
+export default function SpinnerGif() {
+  return (
+    <div className='text-center'>
+      <img className='my-3' src={Spinner} alt="" />
+    </div>
+  )
+}
